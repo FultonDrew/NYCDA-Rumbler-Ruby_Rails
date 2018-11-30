fcbbc31ede61050241d559548144fbcde9601627
FultonDrew <andrewgfulton@gmail.com>
2018-11-30 14:33:45 -0500

    finishing styling

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def after_update_path_for(user)
-    user_profile_path(current_user)
+    user_session_path(current_user)
   end
 
 
